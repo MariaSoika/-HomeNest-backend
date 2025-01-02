9c33d50ad57a4c1fb89a64c124541fabd43807ee
MariaSoika <mariasoika77@gmail.com>
2025-01-02 19:39:44 +0300

    create jpa repository for ResidentialComplex

---
+++ src/main/java/org/example/coursework/repository/ResidentialComplexRepository.java
@@ -0,0 +1,7 @@
+package org.example.coursework.repository;
+
+import org.example.coursework.entity.ResidentialComplex;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResidentialComplexRepository extends JpaRepository<ResidentialComplex, Long> {
+}
