e4490b9ef8008dd4451cbab736ec0ebc9b100a47
MariaSoika <mariasoika77@gmail.com>
2025-02-09 19:29:31 +0300

    done with security

--- src/main/java/org/example/coursework/entity/User.java
+++ src/main/java/org/example/coursework/entity/User.java
@@ -1,46 +1,63 @@
 package org.example.coursework.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.example.coursework.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Builder
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long ID;
+    private long Id;
 
-    @NotNull(message = "Role is mandatory")
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private Role role;
 
-    @NotNull(message = "Password is mandatory")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
-    @Column(name = "password", nullable = false)
-    private String password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "user_favorites",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "apartment_id")
-    )
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-    @ToString.Exclude
-    private List<Apartment> favorite = new ArrayList<>();
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/org/example/coursework/service/UserService.java
+++ src/main/java/org/example/coursework/service/UserService.java
@@ -1,93 +1,196 @@
 package org.example.coursework.service;
 
-import lombok.AllArgsConstructor;
-import org.example.coursework.dto.UserCreateDto;
-import org.example.coursework.dto.UserDto;
+import lombok.RequiredArgsConstructor;
 import org.example.coursework.entity.User;
-import org.example.coursework.exception.UserNotFoundException;
-import org.example.coursework.mapper.UserMapper;
-import org.example.coursework.repository.ApartmentRepository;
+import org.example.coursework.enums.Role;
 import org.example.coursework.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
-@Transactional(readOnly = true)
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository repository;
 
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-
-    private final UserMapper userMapper;
-    private final UserRepository userRepository;
-    private final ApartmentRepository apartmentRepository;
-
-    @CacheEvict(value = "users", allEntries = true)
-    @Transactional
-    public UserDto create(UserCreateDto userCreateDto) {
-        User user = userMapper.toEntity(userCreateDto);
-        logger.info("Created user with ID: {}", user.getID());
-        return userMapper.toDto(userRepository.save(user));
+    /**
+     * Сохранение пользователя
+     *
+     * @return сохраненный пользователь
+     */
+    public User save(User user) {
+        return repository.save(user);
     }
 
-    @CacheEvict(value = "users", key = "#userID")
-    @Transactional
-    public void delete(Long userID) throws UserNotFoundException {
-        if (userRepository.existsById(userID)) {
-            userRepository.deleteById(userID);
-            logger.info("Deleted user with ID: {}", userID);
-            userRepository.flush();
-        } else {
-            logger.info("User with ID: {} does not exist", userID);
-            throw new UserNotFoundException("User with ID " + userID + " does not exist");
+
+    /**
+     * Создание пользователя
+     *
+     * @return созданный пользователь
+     */
+    public User create(User user) {
+        if (repository.existsByUsername(user.getUsername())) {
+            // Заменить на свои исключения
+            throw new RuntimeException("Пользователь с таким именем уже существует");
         }
+
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Пользователь с таким email уже существует");
+        }
+
+        return save(user);
     }
 
-    @CacheEvict(value = "users", key = "#userID")
-    @Transactional
-    public UserDto update(Long userID, UserDto userDto) throws UserNotFoundException {
-        logger.info("Updating user with ID: {}", userID);
-        return userRepository.findById(userID)
-                .map(existingUser -> {
-                    User updateUser = userMapper.toEntity(userDto);
-                    updateUser.setID(userID);
-                    updateUser.setFavorite(userDto.favorite().stream()
-                            .map(apartmentDto -> apartmentRepository.getReferenceById(apartmentDto.ID()))
-                            .collect(Collectors.toList()));
-                    return userMapper.toDto(userRepository.save(updateUser));
-                })
-                .orElseThrow(() -> {
-                    logger.error("User with ID: {} does not exist", userID);
-                    return new UserNotFoundException("User with ID " + userID + " does not exist");
-                });
+    /**
+     * Получение пользователя по имени пользователя
+     *
+     * @return пользователь
+     */
+    public User getByUsername(String username) {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+
     }
 
-    @Cacheable(value = "users", key = "#page + '-' + #size")
-    @Transactional
-    public Page<UserDto> getAll(int page, int size) {
-        logger.info("Getting all users with page {} and size {}", page, size);
-        Pageable pageable = Pageable.ofSize(size).withPage(page);
-        return userRepository.findAll(pageable)
-                .map(userMapper::toDto);
+    /**
+     * Получение пользователя по имени пользователя
+     * <p>
+     * Нужен для Spring Security
+     *
+     * @return пользователь
+     */
+    public UserDetailsService userDetailsService() {
+        return this::getByUsername;
     }
 
-    @Cacheable(value = "users", key = "#userID")
-    @Transactional
-    public UserDto getById(Long userID) throws UserNotFoundException {
-        logger.info("Getting user with ID: {}", userID);
-        return userRepository.findById(userID)
-                .map(userMapper::toDto)
-                .orElseThrow(() -> {
-                    logger.error("User with ID: {} does not exist", userID);
-                    return new UserNotFoundException("User with ID " + userID + " does not exist");
-                });
+    /**
+     * Получение текущего пользователя
+     *
+     * @return текущий пользователь
+     */
+    public User getCurrentUser() {
+        // Получение имени пользователя из контекста Spring Security
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getByUsername(username);
+    }
+
+
+    /**
+     * Выдача прав администратора текущему пользователю
+     * <p>
+     * Нужен для демонстрации
+     */
+    @Deprecated
+    public void getAdmin() {
+        var user = getCurrentUser();
+        user.setRole(Role.ROLE_ADMIN);
+        save(user);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package org.example.coursework.service;
+//
+//import lombok.AllArgsConstructor;
+//import org.example.coursework.dto.UserCreateDto;
+//import org.example.coursework.dto.UserDto;
+//import org.example.coursework.entity.User;
+//import org.example.coursework.exception.UserNotFoundException;
+//import org.example.coursework.mapper.UserMapper;
+//import org.example.coursework.repository.ApartmentRepository;
+//import org.example.coursework.repository.UserRepository;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cache.annotation.CacheEvict;
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.stream.Collectors;
+//
+//@Service
+//@Transactional(readOnly = true)
+//@AllArgsConstructor
+//public class UserService {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+//
+//    private final UserMapper userMapper;
+//    private final UserRepository userRepository;
+//    private final ApartmentRepository apartmentRepository;
+//
+//    @CacheEvict(value = "users", allEntries = true)
+//    @Transactional
+//    public UserDto create(UserCreateDto userCreateDto) {
+//        User user = userMapper.toEntity(userCreateDto);
+//        logger.info("Created user with ID: {}", user.getID());
+//        return userMapper.toDto(userRepository.save(user));
+//    }
+//
+//    @CacheEvict(value = "users", key = "#userID")
+//    @Transactional
+//    public void delete(Long userID) throws UserNotFoundException {
+//        if (userRepository.existsById(userID)) {
+//            userRepository.deleteById(userID);
+//            logger.info("Deleted user with ID: {}", userID);
+//            userRepository.flush();
+//        } else {
+//            logger.info("User with ID: {} does not exist", userID);
+//            throw new UserNotFoundException("User with ID " + userID + " does not exist");
+//        }
+//    }
+//
+//    @CacheEvict(value = "users", key = "#userID")
+//    @Transactional
+//    public UserDto update(Long userID, UserDto userDto) throws UserNotFoundException {
+//        logger.info("Updating user with ID: {}", userID);
+//        return userRepository.findById(userID)
+//                .map(existingUser -> {
+//                    User updateUser = userMapper.toEntity(userDto);
+//                    updateUser.setID(userID);
+//                    updateUser.setFavorite(userDto.favorite().stream()
+//                            .map(apartmentDto -> apartmentRepository.getReferenceById(apartmentDto.ID()))
+//                            .collect(Collectors.toList()));
+//                    return userMapper.toDto(userRepository.save(updateUser));
+//                })
+//                .orElseThrow(() -> {
+//                    logger.error("User with ID: {} does not exist", userID);
+//                    return new UserNotFoundException("User with ID " + userID + " does not exist");
+//                });
+//    }
+//
+//    @Cacheable(value = "users", key = "#page + '-' + #size")
+//    @Transactional
+//    public Page<UserDto> getAll(int page, int size) {
+//        logger.info("Getting all users with page {} and size {}", page, size);
+//        Pageable pageable = Pageable.ofSize(size).withPage(page);
+//        return userRepository.findAll(pageable)
+//                .map(userMapper::toDto);
+//    }
+//
+//    @Cacheable(value = "users", key = "#userID")
+//    @Transactional
+//    public UserDto getById(Long userID) throws UserNotFoundException {
+//        logger.info("Getting user with ID: {}", userID);
+//        return userRepository.findById(userID)
+//                .map(userMapper::toDto)
+//                .orElseThrow(() -> {
+//                    logger.error("User with ID: {} does not exist", userID);
+//                    return new UserNotFoundException("User with ID " + userID + " does not exist");
+//                });
+//    }
+//}
