caf029f85d3c96b8c8a99e7c4132f0c687650269
MariaSoika <mariasoika77@gmail.com>
2025-02-23 12:54:45 +0300

    mappers errors

--- src/main/java/org/example/coursework/mapper/ApartmentMapper.java
+++ src/main/java/org/example/coursework/mapper/ApartmentMapper.java
@@ -7,14 +7,14 @@ import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ApartmentMapper {
-    @Mapping(source = "residentialComplexId", target = "residentialComplex.Id")
+    @Mapping(source = "residentialComplexId", target = "residentialComplex.id")
     Apartment toEntity(ApartmentDto apartmentDto);
 
-    @Mapping(source = "residentialComplex.Id", target = "residentialComplexId")
+    @Mapping(source = "residentialComplex.id", target = "residentialComplexId")
     ApartmentDto toDto(Apartment apartment);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(source = "residentialComplexId", target = "residentialComplex.Id")
+    @Mapping(source = "residentialComplexId", target = "residentialComplex.id")
     Apartment partialUpdate(ApartmentDto apartmentDto, @MappingTarget Apartment apartment);
 
     Apartment toEntity(ApartmentCreateDto apartmentCreateDto);
